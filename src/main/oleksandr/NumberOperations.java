75c3a5f4a6b81c499e35c18370070629bf38949f
Sander1992 <sander92@inbox.ru>
2015-03-27 23:45:43 +0200

    Tasks 86 a&b, 330 updated

--- src/main/oleksandr/NumberOperations.java
+++ src/main/oleksandr/NumberOperations.java
@@ -4,13 +4,12 @@ package oleksandr;
  * Created by Sander on 26.03.2015.
  */
 public class NumberOperations {
-//method for checking number
-    static protected int countNumber(int num) {
+    protected static int countNumber(int num) {
      int  count = Integer.toString(num).length();
         return count;
     }
 
-     static protected int sumOfNumbers(int num) {
+    protected static int sumOfNumbers(int num) {
         int sum = 0;
         while (num != 0) {
             sum = sum + (num % 10);
