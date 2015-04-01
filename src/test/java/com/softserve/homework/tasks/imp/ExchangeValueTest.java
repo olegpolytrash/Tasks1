65d81aaaa65727ebebafc599355e840c3671453a
OlesOnyshchak <oles1719@gmail.com>
2015-04-01 22:52:53 +0300

    add test to all private method in ExchangeValue class

--- src/test/java/com/softserve/homework/tasks/imp/ExchangeValueTest.java
+++ src/test/java/com/softserve/homework/tasks/imp/ExchangeValueTest.java
@@ -12,25 +12,43 @@ import static org.junit.Assert.assertEquals;
  */
 public class ExchangeValueTest {
     private String value;
-    private int actualValue;
-    private int expectedValue = 321;
 
-    private String findReturnedValue() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    private String findPositiveExchangedValue() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         ExchangeValue exchangeValue = new ExchangeValue();
         Method method = exchangeValue.getClass().getDeclaredMethod("exchangePositive", int.class);
         method.setAccessible(true);
         value = (String) method.invoke(exchangeValue, new Object[]{123});
         return value;
     }
+    private String findNegativeExchangedValue() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        ExchangeValue exchangeValue = new ExchangeValue();
+        Method method = exchangeValue.getClass().getDeclaredMethod("exchangeNegative", int.class);
+        method.setAccessible(true);
+        value = (String) method.invoke(exchangeValue, new Object[]{-123});
+        return value;
+    }
 
     @Test
-    public void test() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    public void testExchangePositive() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        int actualValue = 0;
+        int expectedValue = 321;
         try {
-            actualValue = Integer.valueOf(findReturnedValue());
+            actualValue = Integer.valueOf(findPositiveExchangedValue());
         } catch (NoSuchMethodException s) {
             s.printStackTrace();
         }
-        assertEquals("actualValue should be 321",expectedValue, actualValue);
+        assertEquals("actualValue should be 321", expectedValue, actualValue);
+    }
 
+    @Test
+    public void testExchangeNegative() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        int actualValue = 0;
+        int expectedValue = -321;
+        try {
+            actualValue = Integer.valueOf(findNegativeExchangedValue());
+        } catch (NoSuchMethodException s) {
+            s.printStackTrace();
+        }
+        assertEquals("actualValue should be 321", expectedValue, actualValue);
     }
 }
